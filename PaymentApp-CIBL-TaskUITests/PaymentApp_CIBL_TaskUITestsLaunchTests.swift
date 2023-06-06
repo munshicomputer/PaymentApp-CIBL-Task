6ef465c0c931084a27a25db03163cad489b5488c
SHAMIM MUNSHI <munshi6668@gmail.com>
2023-06-07 00:50:01 +0600

    initial Commit Project Created

---
+++ PaymentApp-CIBL-TaskUITests/PaymentApp_CIBL_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PaymentApp_CIBL_TaskUITestsLaunchTests.swift
+//  PaymentApp-CIBL-TaskUITests
+//
+//  Created by Md Hosne Mobarok on 7/6/23.
+//
+
+import XCTest
+
+final class PaymentApp_CIBL_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
